7d1ce60dc9a86e6c17b14eb845e1abb94135fe17
Dhruvil <dhruvil@192.168.1.11>
2026-08-02 22:22:58 +0530

    Add robots.ts for Googlebot and AdSense crawler

---
+++ app/robot.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Mediapartners-Google',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://www.freelancecalcsuite.online/sitemap.xml',
+  };
+}
